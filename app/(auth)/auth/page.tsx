35842ff569ca521beabacee3d1b0981233ca1574
nadir <nadirelserhid@gmail.com>
2024-08-13 12:51:47 +0300

    the billing page ready

--- app/(auth)/auth/page.tsx
+++ app/(auth)/auth/page.tsx
@@ -1,7 +1,30 @@
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+  
 const AuthPage = () => {
-    return ( <>
-    <div></div>
-    </> );
+    return (<div className="font-sans justify-center  justify-items-center">
+    <h1 className="m-[50px] " >login</h1>
+    <div>HELLO FROM AUTH PAGE</div>
+    <DropdownMenu>
+  <DropdownMenuTrigger className=" bg-slate-200 ">Open</DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>Profile</DropdownMenuItem>
+    <DropdownMenuItem>Billing</DropdownMenuItem>
+    <DropdownMenuItem>Team</DropdownMenuItem>
+    <DropdownMenuItem>Subscription</DropdownMenuItem>
+    <DropdownMenuItem>log out</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+
+    </div> );
 }
  
 export default AuthPage;
